74ed8ddf4cb7748d70e104ca453a25f01107332e
Ayush <ayushyadav935@gmail.com>
2021-03-06 21:09:22 +0530

    login added

---
+++ login+signup/script.js
@@ -0,0 +1,11 @@
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+ });
+ 
+// var create = document.getElementsByClassName('register-form').addEventListener
+// ('submit',adduser);
+
+// function adduser(e){
+//     e.preventDefault();
+//     console.log("hii")
+// }
